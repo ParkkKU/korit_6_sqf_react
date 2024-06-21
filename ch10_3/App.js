1a2d84f182ad948b825ea271e7790091e0595f1f
ParkkKU <pku02089@gmail.com>
2024-06-21 17:37:22 +0900

    240621

--- ch10_3/App.js
+++ ch10_3/App.js
@@ -2,6 +2,9 @@ import { useRef, useState } from "react";
 import "./App.css"
 
 function App() {
+
+    const Swal = require('sweetalert2');
+
     const emptyUser = {
         username: "",
         password: "",
@@ -47,8 +50,29 @@ function App() {
         });
     }
 
-    const handleDeleteCheck = (e) => {
-        setUserList(userList => [...userList.filter((user, index) => index !== parseInt(e.target.value))]);
+    const handleEditClick = (key, index) => {
+        // Swal.fire({
+        //     title: "사용자 수정",
+        //     input: "text"
+        // })
+        console.log(key);
+        console.log(index);
+    }
+
+    const handleDeleteClick = (e) => {
+        Swal.fire({
+            title: "사용자 삭제",
+            text: "해당 사용자를 삭제하시겠습니까?",
+            icon: "question",
+            showCancelButton: true,
+            confirmButtonText: "삭제",
+            confirmButtonColor: "red",
+            cancleButtonText: "취소"
+        }).then(result => {
+            if(result.isConfirmed) {
+                setUserList(userList => [...userList.filter((user, index) => index !== parseInt(e.target.value))]);
+            }
+        })
     }
 
 
@@ -78,7 +102,8 @@ function App() {
                     <th>username</th>
                     <th>password</th>
                     <th>name</th>
-                    <th>삭제</th>
+                    <th>edit</th>
+                    <th>delete</th>
                 </tr>
             </thead>
             <tbody>
@@ -87,10 +112,15 @@ function App() {
                     return (
                         <tr key={index}>
                             <td>{index + 1}</td>
-                            <td>{username}</td>
-                            <td>{password}</td>
-                            <td>{name}</td>
-                            <button name="delete" onClick={(userList) => handleDeleteCheck(index + 1)}>삭제</button>
+                            <td name="username" onClick={() => handleEditClick("username", index)}>{username}</td>
+                            <td name="password" onClick={() => handleEditClick("password", index)}>{password}</td>
+                            <td name="name" onClick={() => handleEditClick("name", index)}>{name}</td>
+                            <td>
+                                <button value={index}>수정</button>
+                            </td>
+                            <td>
+                                <button onClick={handleDeleteClick} value={index}>삭제</button>
+                            </td>                        
                         </tr>
                     );
                 })
---
+++ ch11/App.js
@@ -0,0 +1,14 @@
+import "./App.css"
+import DataTable from "./components/DataTable/DataTable";
+
+function App() {
+    return (
+        <div>
+            <h1 className="container">상품 관리 페이지
+            <DataTable />
+            </h1>
+        </div>
+    )
+}
+
+export default App;
