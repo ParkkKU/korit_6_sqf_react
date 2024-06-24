d6206e3128100f25f2258356b64d8783d956db07
ParkkKU <pku02089@gmail.com>
2024-06-24 17:51:14 +0900

    240624

--- ch11/components/DataTableHeader/DataTableHeader.jsx
+++ ch11/components/DataTableHeader/DataTableHeader.jsx
@@ -1,28 +1,107 @@
-import { useRef } from "react";
-import "./style.css"
+import { useRef, useState } from "react";
+import "./style.css";
+import Swal from "sweetalert2";
 
-function DataTableHeader({ mode, setMode, setProducts }) {
+function DataTableHeader({ mode, setMode, setProducts, setDeleting }) {
+
+    const emptyProduct = {
+        id: "",
+        productName: "",
+        size: "",
+        color: "",
+        price: ""
+    };
 
     const inputRef = {
         productName: useRef(),
         size: useRef(),
         color: useRef(),
         price: useRef()
+    };
+
+    const [ inputData, setInputData ] = useState({ ...emptyProduct });
+
+    const handleInputChange = (e) => {
+        setInputData(inputData => ({
+            ...inputData,
+            [e.target.name]: e.target.value
+        }));
+    }
+
+    const handleInputKeyDown = (e) => {
+        if(e.keyCode === 13) {
+            if(e.target.name === "productName") {
+                inputRef.size.current.focus();
+            }
+            if(e.target.name === "size") {
+                inputRef.color.current.focus();
+            }
+            if(e.target.name === "color") {
+                inputRef.price.current.focus();
+            }
+            if(e.target.name === "price") {
+                handleSubmitClick();
+                inputRef.productName.current.focus();
+            }
+        }
     }
 
     const handleChangeModeClick = (e) => {
         setMode(parseInt(e.target.value));
     }
 
-    const handleSubmitClick = () => {
+    const handleSubmitClick = (e) => {
         if(mode === 1) {
-            alert("상품추가");
+            setProducts(products => {
+                const productIds = products.map(product => product.id);
+                const maxId = 
+                    productIds.length === 0 
+                        ? 0 
+                        : Math.max.apply(null, productIds);
+
+                return [ ...products, { ...inputData, id: maxId + 1 } ];
+            });
+            Swal.fire({
+                title: "상품 정보 추가 완료",
+                icon: "success",
+                position: "top-end",
+                showConfirmButton: false,
+                timer: 2000
+            });
+            resetMode();
         }
         if(mode === 2) {
+            switch(e.target.ref) {
+                case inputRef.productName:
+                    inputRef.size.current.focus();
+                    break;
+                case inputRef.size:
+                    inputRef.color.current.focus();
+                    break;
+                case inputRef.color:
+                    inputRef.price.current.focus();
+                    break;
+                case inputRef.price:
+                    inputRef.productName.current.focus();
+                    setProducts( products => [...products, inputData]);
+                    setInputData({...emptyProduct});
+                    break;
+                default:
+            }
             alert("상품수정");
         }
         if(mode === 3) {
-            alert("상품삭제");
+            Swal.fire({
+                title: "상품 정보 삭제",
+                text: "정말로 삭제 하시겠습니까?",
+                showCancelButton: true,
+                confirmButtonText: "삭제",
+                cancelButtonText: "취소"
+            }).then(result => {
+                if (result.isConfirmed) {
+                    setDeleting(true);
+                }
+            });
         }
         resetMode();
     }
@@ -33,57 +112,72 @@ function DataTableHeader({ mode, setMode, setProducts }) {
 
     const resetMode = () => {
         setMode(0);
+        setInputData({ ...emptyProduct });
     }
 
-    
-    const handleProductAdd = (e) => {
-        const { productName, size, color, price } = inputRef;
-        if(e.keyCode === 13) {
-            switch(e.target.name) {
-                case "productName":
-                    size.current.focus();
-                    break;
-                case "size":
-                    color.current.focus();
-                    break;
-                case "color":
-                    price.current.focus();
-                    break;
-                case "price":
-                    productName.current.focus();
-                    // setProducts(productList => [productList, ...]);
-                    break;
-                default:
-            }
-        }
-    }
-    
     return (
         <header className="table-header">
             <div className="input-group">
-                <input name="productName" type="text" disabled={mode === 0 || mode === 3} placeholder="상품명" ref={inputRef.productName}/>
-                <input name="size" type="text" disabled={mode === 0 || mode === 3} placeholder="사이즈" ref={inputRef.size} />
-                <input name="color" type="text" disabled={mode === 0 || mode === 3} placeholder="색상" ref={inputRef.color} />
-                <input name="price" type="text" disabled={mode === 0 || mode === 3} placeholder="가격" ref={inputRef.price}/>
+                <input 
+                    type="text" 
+                    disabled={mode === 0 || mode === 3} 
+                    name="productName"
+                    value={inputData.productName}
+                    placeholder="상품명" 
+                    onChange={handleInputChange}
+                    onKeyDown={handleInputKeyDown}
+                    ref={inputRef.productName}
+                    autoFocus 
+                />
+                <input 
+                    type="text" 
+                    disabled={mode === 0 || mode === 3} 
+                    name="size"
+                    value={inputData.size}
+                    placeholder="사이즈" 
+                    onChange={handleInputChange}
+                    onKeyDown={handleInputKeyDown}
+                    ref={inputRef.size}
+                />
+                <input 
+                    type="text" 
+                    disabled={mode === 0 || mode === 3}
+                    name="color"
+                    value={inputData.color}
+                    placeholder="색상" 
+                    onChange={handleInputChange}
+                    onKeyDown={handleInputKeyDown}
+                    ref={inputRef.color}
+                />
+                <input 
+                    type="text" 
+                    disabled={mode === 0 || mode === 3} 
+                    name="price"
+                    value={inputData.price}
+                    placeholder="가격" 
+                    onChange={handleInputChange}
+                    onKeyDown={handleInputKeyDown}
+                    ref={inputRef.price}
+                />
             </div>
             <div>
-            {
-                !mode && (
-                    <div className="button-group">
-                        <button name="add" onClick={handleChangeModeClick} value={1} >추가</button>
-                        <button name="edit" onClick={handleChangeModeClick} value={2} >수정</button>
-                        <button name="delete" onClick={handleChangeModeClick} value={3} >삭제</button>
-                    </div>
-                )
-            }
-            {
-                !!mode && (
-                    <div className="button-group">
-                        <button onClick={handleSubmitClick}>확인</button>
-                        <button onClick={handleCancelClick}>취소</button>
-                    </div>
-                )
-            }
+                {
+                    !mode && (
+                        <div className="button-group">
+                            <button onClick={handleChangeModeClick} value={1} >추가</button>
+                            <button onClick={handleChangeModeClick} value={2} >수정</button>
+                            <button onClick={handleChangeModeClick} value={3} >삭제</button>
+                        </div>
+                    )
+                }
+                {
+                    !!mode && (
+                        <div className="button-group">
+                            <button onClick={handleSubmitClick} >확인</button>
+                            <button onClick={handleCancelClick} >취소</button>
+                        </div>
+                    )
+                }
             </div>
         </header>
     );
